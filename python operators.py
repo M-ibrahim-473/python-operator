dde195de4f5891b39905334cfbf6e506b36e0675
Muhammad Ibrahim <ibrahimjutt4733@gmail.com>
2025-07-04 12:07:32 +0500

    Add operators.py via upload
    This is my first code which I written myself

---
+++ python operators.py
@@ -0,0 +1,44 @@
+#Artimatic opertor
+a=5
+b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)#remainder
+# print(a**b)#a^
+#Reltion opertors
+a=40
+b=30
+# print(a==b)#flase
+# print(a!=b)#true
+# print(a<=b)#flase
+# print(a>=b)#true
+# print(a<b)#flase
+# print(a>b)#true
+#Assignment operator 
+num=20
+# num-=10
+# num+=10
+# num*=10
+# num/=10
+# num%=10
+# num**=10
+# print('num is:',num)#0
+#Locial operator
+a=50
+b=25
+#NOT 
+# print(not False)
+# print(not(a>b)) 
+#AND 
+val1=True
+val2=False
+#print('And operator is :',val1 and val2)
+#OR
+val1=True
+val2=False
+#print('or operator is :',val1 or val2)
+#print('or operator is :',(a==b) or (a>b))
+x=int(5)
+print(x)
